1e36a01fc28e2e2118c72c435ebd84ce7c0ec4f4
Mischa Tobias <mischa.tobias4b@gmail.com>
2024-09-14 17:00:02 -0600

    correct navbar message

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
-        Asociaciones
+        Hero App
       </Link>
 
       <div className="navbar-collapse">
